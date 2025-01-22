d51dc3cb177aea6a55bf1e92fc9194d9e9fdf35f
nallyd <82608136+nallyd@users.noreply.github.com>
2025-01-21 19:43:00 -0500

    implement grid creation, sizing, and css for hover
    Implements the new grid option, and changes the way we size the gridd so
    that we can ensure a square across platforms

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,6 +1,10 @@
 console.log("We're live!");
 
+// Playing field
 const field = document.querySelector(".field");
+// Reset options
+const resetSizeInput = document.querySelector("#size");
+const resetSizeButton = document.querySelector(".options button");
 
 function createSquareGrid(container, size) {
     const grid = document.createElement("div");
@@ -12,20 +16,39 @@ function createSquareGrid(container, size) {
         for (let j = 0; j < size; j++) {
             const box = document.createElement("div");
             box.setAttribute("class", "grid-box");
+            box.addEventListener("mouseenter", () => {
+                onHoverEnter(box);
+            });
             row.appendChild(box);
         }
 
         grid.appendChild(row);
     }
 
-    container.appendChild(grid);
+    container.prepend(grid);
     return;
 }
 
 function removeGrid(container) {
-    console.log(`Removing ${container.firstElementChild} from the field`);
+    console.log(`[DEBUG]: Removing ${container.firstElementChild} from the field`);
     container.removeChild(container.firstElementChild);
     return;
 }
+
+function onHoverEnter(div) {
+    div.setAttribute("class", "grid-box hovered");
+    return;
+}
+
+resetSizeButton.addEventListener("click", () => {
+    let size = Number(resetSizeInput.value);
+    console.log(size);
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert("Grid values are [1, 100] inclusive.");
+    } else {
+        removeGrid(field);
+        createSquareGrid(field, size);
+    }
+});
 
 createSquareGrid(field, 16);
